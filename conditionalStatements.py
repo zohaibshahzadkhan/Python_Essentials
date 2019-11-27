dfa49553c0574708b7f955ffaa95db230c04c05f
zohaibshahzadkhan <zohaibshahzad16@gmail.com>
2019-11-27 10:36:03 +0500

    conditional statements in python

---
+++ conditionalStatements.py
@@ -0,0 +1,8 @@
+basic_pay = 500000;
+title = 'Software Engineer';
+if basic_pay == 500000 and title == 'Software Engineer':
+  print('your basic pay is ' + str(basic_pay));
+elif basic_pay == 400000 and  title != 'Software Engineer':
+  print('your basic pay is 400000');
+else:
+  print('your basic pay is not equals to ' + str(basic_pay)) 
